e909f6bd55209ca322b9712d4348c49b87306977
Petter W. Rognhaug <prognhaug@gmail.com>
2024-07-07 20:35:35 +0200

    add company details to setup page

--- src/components/CompanyDetails.tsx
+++ src/components/CompanyDetails.tsx
@@ -1,21 +1,25 @@
-import React from "react";
 import { Company } from "../models/types";
 
 const CompanyDetails: React.FC<{ company: Company }> = ({ company }) => {
   return (
-    <div>
-      <h3>Company Details</h3>
-      <table>
-        <tbody>
-          <tr>
-            <td>ID</td>
-            <td>{company._id}</td>
+    <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+      <h2 className="text-xl font-semibold leading-tight text-white">
+        Company Details
+      </h2>
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <tbody className="bg-white dark:bg-gray-800">
+          <tr className="border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td className="px-6 py-4">ID</td>
+            <td className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+              {company.companyID}
+            </td>
           </tr>
-          <tr>
-            <td>Name</td>
-            <td>{company.name}</td>
+          <tr className="border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td className="px-6 py-4">Name</td>
+            <td className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+              {company.name}
+            </td>
           </tr>
-          {/* Render other company details as needed */}
         </tbody>
       </table>
     </div>
--- src/pages/Setup/Setup.tsx
+++ src/pages/Setup/Setup.tsx
@@ -1,9 +1,21 @@
-import NavBar from "../../components/NavBar"; // Adjust the import path based on your project structure
+import NavBar from "../../components/NavBar";
+import CompanyDetails from "../../components/CompanyDetails";
+import { CompanyContext } from "../../context/CompanyContext";
+import { useContext } from "react";
 
 const Setup = () => {
+  const { company } = useContext(CompanyContext);
   return (
-    <div className="flex">
-      <NavBar />
+    <div className="flex min-h-screen bg-gray-700">
+      <div className="w-64">
+        <NavBar />
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <h1 className="text-white mt-4 mb-2">Setup Page</h1>
+        <div className="w-full p-4">
+          {company && <CompanyDetails company={company} />}
+        </div>
+      </div>
     </div>
   );
 };
