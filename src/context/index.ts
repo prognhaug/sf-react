4ca80db2fbf810ced411fe5a427b47cc3b8994c9
Petter W. Rognhaug <prognhaug@gmail.com>
2024-07-15 20:29:35 +0200

    delete unused files

--- src/context/index.ts
+++ src/context/index.ts
@@ -2,4 +2,3 @@ export { CompaniesContext } from "./CompaniesContext";
 export { CompanyContext } from "./CompanyContext";
 export { ConnectionContext } from "./ConnectionContext";
 export { InstanceContext } from "./InstanceContext";
-export { SelectedCompanyContext } from "./SelectedCompanyContext";
